c5e1056357912fbb590047610763d655f7e589b3
greymag <greymag@gmail.com>
2020-07-21 18:54:27 +0300

    Game tab.

---
+++ lib/screens/game/game_tab.dart
@@ -0,0 +1,32 @@
+import 'package:WhatWhereWhenMaster/application/routes.dart';
+import 'package:flutter/material.dart';
+
+import 'package:WhatWhereWhenMaster/models/game_data/game_data.dart';
+import 'package:WhatWhereWhenMaster/screens/round_list/round_list_screen.dart';
+
+/// Game tab for a [GameScreen].
+class GameTab extends StatelessWidget {
+  final GameData game;
+
+  const GameTab({Key key, @required this.game})
+      : assert(game != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      onGenerateRoute: (settings) {
+        if (settings.name == Navigator.defaultRouteName) {
+          return AppRoutes.create(
+            builder: (context) => RoundListScreen(rounds: game.rounds),
+            settings: settings,
+          );
+        }
+
+        return Navigator.of(context, rootNavigator: true)
+            .widget
+            .onGenerateRoute(settings);
+      },
+    );
+  }
+}
