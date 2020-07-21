17e58fb9bde8f40b26e4f7a56fac2410b9ab8d2f
greymag <greymag@gmail.com>
2020-07-21 18:28:36 +0300

    Named route for the game screen.

--- lib/application/routes.dart
+++ lib/application/routes.dart
@@ -1,16 +1,22 @@
+import 'package:WhatWhereWhenMaster/screens/game/game_screen.dart';
 import 'package:WhatWhereWhenMaster/screens/home/home_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// App routes.
 class AppRoutes {
+  /// See [HomeScreen].
   static const home = "/";
+
+  /// See [GameScreen].
+  static const game = "/game";
 
   /// Строит основной контент по пути.
   static Widget buildByRoute(BuildContext context, String route, Object args) {
     switch (route) {
       case AppRoutes.home:
         return HomeScreen();
+      case AppRoutes.game:
+        return GameScreen();
     }
 
     throw Exception('Unknown route: $route');
