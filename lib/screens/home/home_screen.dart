4916ce077281cee779443adbf0393609670c8551
greymag <greymag@gmail.com>
2020-07-21 18:28:55 +0300

    Demo button.

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,9 +1,24 @@
+import 'package:WhatWhereWhenMaster/application/routes.dart';
 import 'package:flutter/material.dart';
 
 /// Home screen.
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed(AppRoutes.game);
+          },
+          child: Icon(
+            Icons.play_arrow,
+            size: 100,
+          ),
+          color: Colors.cyan,
+          padding: const EdgeInsets.all(20),
+        ),
+      ),
+    );
   }
 }
