59bbafd6045002852335f544253d9fd7fe4e6241
greymag <greymag@gmail.com>
2020-08-11 18:53:42 +0300

    Question view.

--- lib/widgets/question/question_view.dart
+++ lib/widgets/question/question_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 /// Widget for display question
 class QuestionView extends StatelessWidget {
   static const _answerPadding = 40.0;
+  static const _mainTextStyle = TextStyle(fontSize: 24);
 
   final Question question;
   final bool showAnswer;
@@ -28,22 +29,55 @@ class QuestionView extends StatelessWidget {
   }
 
   Widget _buildIntro(BuildContext context, Question question) {
-    return Text(question.intro);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Text(
+        question.intro,
+        style: _mainTextStyle.copyWith(fontWeight: FontWeight.bold),
+      ),
+    );
   }
 
   Widget _buildQuestionText(BuildContext context, Question question) {
-    return Text(question.text);
+    return Text(
+      question.text,
+      style: _mainTextStyle,
+    );
   }
 
   Widget _buildAnswer(BuildContext context, Question question) {
     final loc = AppLocalizations.of(context);
     return Column(
       children: [
-        Text(loc.answerTitle),
-        Text(question.answer),
+        Text(
+          loc.answerTitle,
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          question.answer,
+          style: _mainTextStyle,
+        ),
         if (question.hasAlternativeAnswer)
-          Text(loc.getAlternativeAnswer(question.answerAlt)),
-        if (question.hasComment) Text(loc.getCommentText(question.comment)),
+          Text(
+            loc.getAlternativeAnswer(question.answerAlt),
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        if (question.hasComment)
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              loc.getCommentText(question.comment),
+              style: TextStyle(
+                fontSize: 18,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
       ],
     );
   }
