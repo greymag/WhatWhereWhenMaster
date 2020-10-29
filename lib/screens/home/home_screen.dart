199ba1ec104f2ba76385173188fc2ffcba5b35c8
greymag <greymag@gmail.com>
2020-10-29 19:36:11 +0300

    Shows loaded games list on home screen.

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,24 +1,51 @@
-import 'package:WhatWhereWhenMaster/application/routes.dart';
+import 'package:WhatWhereWhenMaster/blocs/home/home_bloc.dart';
+import 'package:WhatWhereWhenMaster/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:innim_lib/innim_lib.dart';
+import 'package:innim_ui/innim_ui.dart';
 
 /// Home screen.
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.of(context).pushNamed(AppRoutes.game);
+      body: BlocProvider(
+        create: (context) =>
+            HomeBloc(context.bloc(), context.repository())..started(),
+        child: BlocBuilder<HomeBloc, HomeState>(
+          builder: (context, state) {
+            if (state is HomeLoadSuccess) {
+              return _buildBody(context, state.games);
+            }
+
+            // TODO: navigate to the auth form when HomeSignInRequired
+            debugAssertState(
+                state is HomeInitial ||
+                    state is HomeLoadInProgress ||
+                    state is HomeSignInRequired,
+                state);
+            return LoadingWidget();
           },
-          child: Icon(
-            Icons.play_arrow,
-            size: 100,
-          ),
-          color: Colors.cyan,
-          padding: const EdgeInsets.all(20),
         ),
       ),
     );
+  }
+
+  Widget _buildBody(BuildContext context, List<GameDataEntry> games) {
+    // TODO: state if no games
+    return ListView.builder(
+      itemCount: games.length,
+      itemBuilder: (context, index) => _buildListItem(context, games[index]),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, GameDataEntry entry) {
+    return Text(entry.title);
   }
 }
